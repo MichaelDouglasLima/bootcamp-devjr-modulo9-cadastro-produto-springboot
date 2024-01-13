53957f7714cdcd12730751659acf564d697aba11
Michael Douglas <michael.lima8@fatec.sp.gov.br>
2024-01-13 08:55:01 -0300

    Update Category

--- src/main/java/com/abutua/productbackend/models/Product.java
+++ src/main/java/com/abutua/productbackend/models/Product.java
@@ -6,18 +6,18 @@ public class Product {
     private int id;
     private String name;
     private String description;
-    private int idCategory;
+    private Category category;
     private boolean promotion;
     private boolean newProduct;
     private double price;
 
     // Métodos Construtores
-    public Product(int id, String name, String description, int idCategory, boolean promotion, boolean newProduct,
+    public Product(int id, String name, String description, Category category, boolean promotion, boolean newProduct,
             double price) {
         this.id = id;
         this.name = name;
         this.description = description;
-        this.idCategory = idCategory;
+        this.category = category;
         this.promotion = promotion;
         this.newProduct = newProduct;
         this.price = price;
@@ -66,12 +66,12 @@ public class Product {
         this.description = description;
     }
 
-    public int getIdCategory() {
-        return idCategory;
+    public Category getCategory() {
+        return category;
     }
 
-    public void setCategory(int idCategory) {
-        this.idCategory = idCategory;
+    public void setCategory(Category category) {
+        this.category = category;
     }
 
     public boolean isPromotion() {
