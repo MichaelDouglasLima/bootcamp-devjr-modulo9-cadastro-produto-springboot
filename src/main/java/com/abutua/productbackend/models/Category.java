24b32d7a8c9bf331a85a4a2159ca512d0a7245dd
Michael Douglas <michael.lima8@fatec.sp.gov.br>
2024-01-13 07:58:21 -0300

    First Commit

---
+++ src/main/java/com/abutua/productbackend/models/Category.java
@@ -0,0 +1,36 @@
+package com.abutua.productbackend.models;
+
+public class Category {
+
+    // Atributos
+    private int id;
+    private String name;
+
+    // Métodos Construtores
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Category() {
+        
+    }
+
+    // Métodos
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
